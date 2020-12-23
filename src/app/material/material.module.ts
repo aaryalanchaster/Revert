d057044fa0916fb84283e160e5db5ed97bfab9b4
aarya <aaryalanchaster@gmail.com>
2020-12-23 22:49:03 +0530

    Co-authored-by: Dixita Shetty <dixitashetty@outlook.com> Co-authored-by: jeet patel <pateljeet878@gmail.com>

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 const material = [
   MatToolbarModule,
   MatSidenavModule,
@@ -16,7 +17,8 @@ const material = [
   MatIconModule,
   MatDividerModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatTooltipModule
 
 ];
 @NgModule({
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   opened = true;
-  // arrow = keyboard
+  arrow = "keyboard_arrow_right";
+  tip = "Close";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onToolbarMenuToggle() {
+    console.log('On toolbar toggled', this.opened);
+    this.opened = !this.opened;
+    if (!this.opened) {
+      this.arrow = "keyboard_arrow_right";
+      this.tip = "Open";
+    } else {
+      this.arrow = "keyboard_arrow_left";
+      this.tip = "Close";
+    }
   }
 }
